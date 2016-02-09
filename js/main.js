c2f8417d72b1cff33bbc410ca214010bca1c19e5
Bacon_Space <thestreamstats@gmail.com>
2016-02-09 11:45:33 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -10,19 +10,19 @@ $(document).ready(function(event){
 });
 function getData(){
     $.get('https://player.me/api/v1/users/?username=true' + $('#inputText').val(), "", function(data){
-         var avatar = "https://player.me/api/v1/users/?username=true" + data['id']['avatar'] + "/avatarId";
+         var avatar = "https://player.me/api/v1/users/" + data['username']['avatar'] + "/?username=true";
          var username = data['id']['username'];
-         var followers = data['followers'];
+         var followers = data['followers_count'];
          if(data['type'] != null){
             var lastPlayed = data['type']['name'];
          }else{
             var lastPlayed = "None";
          }
-         var joined = data['createdAt'];
+         var joined = data['created_at'];
          var html = '<center><img src="' + avatar + '"width="100px" height="100px" style="border:3px solid white">';
          html += '<h1>' + username + '</h1>';
          html += '<br><b>Followers: </b>' + followers;
-         html += '<br><b>Total Views: </b>' + totalViews;
+         html += '<br><b>Following: </b>' + totalViews;
          html += "<br>";
          if(online){
              html += '<br><b><font color="green"><a href="https://beam.pro/' + username + '">Online</a></font></b>';
